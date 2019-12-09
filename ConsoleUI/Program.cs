a7620534969d65cc91f7ececc27704915d526a70
david colvin <davidmcolvin@gmail.com>
2019-12-08 21:01:00 -0500

    Add an example of both Func and Action delegates

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleUI
     {
       PopulateCartWithDemoData();
 
-      Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert):C2}");
+      Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert, CalculateLeveledDiscount, AlertUser):C2}");
 
       Console.WriteLine();
       Console.Write("Please press any key to exit the application...");
@@ -23,9 +23,34 @@ namespace ConsoleUI
 
     }
 
+    private static void AlertUser(string message)
+    {
+      Console.WriteLine(message);
+    }
+
     private static void SubTotalAlert(decimal subTotal)
     {
       Console.WriteLine($"The subtotal is {subTotal:C2}");
+    }
+
+    private static decimal CalculateLeveledDiscount(List<ProductModel> items, decimal subTotal)
+    {
+      if (subTotal > 100)
+      {
+        return subTotal * 0.80M;
+      }
+      else if (subTotal > 50)
+      {
+        return subTotal * 0.85M;
+      }
+      else if (subTotal > 10)
+      {
+        return subTotal * 0.95M;
+      }
+      else
+      {
+        return subTotal;
+      }
     }
 
     private static void PopulateCartWithDemoData()
